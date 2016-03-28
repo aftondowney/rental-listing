51b81f6ab1f4507422dffa786cc415ac5372abba
Afton Downey and Bri Popson <bpopson@gmail.com>
2016-03-28 10:22:06 -0700

    add imageHide to rental-tile

--- app/components/rental-tile.js
+++ app/components/rental-tile.js
@@ -1,5 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  ifImageShowing: false
+  isImageShowing: false,
+ actions: {
+    imageShow: function() {
+      this.set('isImageShowing', true);
+    },
+    imageHide: function() {
+      this.set('isImageShowing', false);
+    }
+  }
 });
